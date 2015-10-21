7734c758741571f3edaf12796b9c4a54233e0d00
tangweichang <tangweichangjlu@gmail.com>
2015-10-20 20:43:04 -0400

    Initial Commit

---
+++ Touch ID Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Touch ID Demo
+//
+//  Created by TangWeichang on 10/20/15.
+//  Copyright © 2015 TangWeichang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
