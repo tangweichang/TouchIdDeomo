a5e20eeb3efe839b509e2efbd60f4877cffa0096
tangweichang <tangweichangjlu@gmail.com>
2015-10-20 20:53:34 -0400

    3D Touch Implematation

--- Touch ID Demo/ViewController.swift
+++ Touch ID Demo/ViewController.swift
@@ -7,12 +7,34 @@
 //
 
 import UIKit
+import LocalAuthentication
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let authenticationContext = LAContext()
+        var error: NSError?
+        if authenticationContext.canEvaluatePolicy(.DeviceOwnerAuthenticationWithBiometrics, error: &error) {
+            authenticationContext.evaluatePolicy(.DeviceOwnerAuthenticationWithBiometrics, localizedReason: "We need to know who you are...") { (success, error) -> Void in
+                if success {
+                    // User has authenticated
+                } else {
+                    if let error = error {
+                        // There was an error
+                    } else {
+                        //User did not authenticate
+                    }
+                }
+            }
+
+            
+        } else {
+            // No touch id available
+        }
+        
     }
 
     override func didReceiveMemoryWarning() {
